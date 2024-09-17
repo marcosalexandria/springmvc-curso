b186472431f08da59fd502f7102579f2612d8159
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-09-17 10:10:10 -0300

    Formulário de Criação com Thymeleaf

--- springprojeto/src/main/java/com/cursomvc/springprojeto/controllers/ProfessorController.java
+++ springprojeto/src/main/java/com/cursomvc/springprojeto/controllers/ProfessorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cursomvc.springprojeto.models.Professor;
+import com.cursomvc.springprojeto.models.enums.StatusProfessor;
 import com.cursomvc.springprojeto.repositories.ProfessorRepository;
 
 @Controller
@@ -24,6 +25,14 @@ public class ProfessorController {
 		ModelAndView mv = new ModelAndView("professor/index");	// a pagina renderizada e manipulada
 		mv.addObject("professores", list);
 		
+		return mv;
+	}
+	
+	@GetMapping("/professores/novo")
+	public ModelAndView inserirProfessor() {
+		
+		ModelAndView mv = new ModelAndView("professor/novo");
+		mv.addObject("statusProfessor", StatusProfessor.values());
 		return mv;
 	}
 }
