62809f50cd596a05828d7cfdce3a6826034318ab
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-09-18 16:28:46 -0300

    Action de Atualização de Registros no Banco

--- springprojeto/src/main/java/com/cursomvc/springprojeto/controllers/ProfessorController.java
+++ springprojeto/src/main/java/com/cursomvc/springprojeto/controllers/ProfessorController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cursomvc.springprojeto.models.Professor;
@@ -20,6 +21,7 @@ import com.cursomvc.springprojeto.utils.CaminhoRequestUtil;
 import jakarta.validation.Valid;
 
 @Controller
+@RequestMapping(value = "/professores")
 public class ProfessorController {
 	
 	@Autowired
@@ -27,7 +29,7 @@ public class ProfessorController {
 	@Autowired
 	ModelMapper mapper;
 	
-	@GetMapping("/professores")
+	@GetMapping
 	public ModelAndView index() {
 
 		List<Professor> list = professorRepository.findAll();
@@ -39,7 +41,7 @@ public class ProfessorController {
 		return mv;
 	}
 	
-	@GetMapping("/professores/novo")
+	@GetMapping("/novo")
 	public ModelAndView inserirProfessor(PostProfessorDTO postProfessorDTO) {
         
 		ModelAndView mv = new ModelAndView("professor/novo");
@@ -48,7 +50,7 @@ public class ProfessorController {
 		return mv;
 	}
 	
-	@PostMapping("/professores")
+	@PostMapping
 	public ModelAndView create(@Valid PostProfessorDTO postProfessorDTO, //Vai ser avaliado
 								BindingResult bindingResult) { //objeto que registra se houve algum esso na requisicao
         
@@ -71,7 +73,7 @@ public class ProfessorController {
 		
 	}
 	
-	@GetMapping("/professores/{id}")
+	@GetMapping("/{id}")
 	public ModelAndView detalhe(@PathVariable Long id) {
 		
 		try {
@@ -91,5 +93,56 @@ public class ProfessorController {
 			return new ModelAndView("redirect:/professores");
 		}
 
+	}
+	
+	@GetMapping("/{id}/editar")
+	public ModelAndView editar(@PathVariable Long id, PostProfessorDTO postProfessorDTO) {
+		
+		try {
+		Professor professor = professorRepository.findById(id).get();
+		
+		ModelAndView mv = new ModelAndView("/professor/editar");
+		mv.addObject("caminho", CaminhoRequestUtil.caminho());
+		mv.addObject("postProfessorDTO", mapper.map(professor, postProfessorDTO.getClass()));
+		mv.addObject("listaStatusProfessor", StatusProfessor.values());
+		mv.addObject("professorId", professor.getId());
+		
+		return mv;
+		
+		} catch (Exception e) {
+			System.out.println("####################################");
+			System.out.println(e.getMessage());
+			System.out.println("####################################");
+			return new ModelAndView("redirect:/professores");
+		}
+		
+	}
+	
+	@PostMapping("/{id}")
+	public ModelAndView atualizar(@PathVariable Long id, @Valid PostProfessorDTO postProfessorDTO, BindingResult bindingResult) {
+		
+		try {
+			
+			if(bindingResult.hasErrors()) {
+				ModelAndView mv = new ModelAndView();
+				mv =  new ModelAndView("professor/novo");
+				mv.addObject("caminho", CaminhoRequestUtil.caminho());
+				mv.addObject("listaStatusProfessor", StatusProfessor.values());
+				return mv;
+			}
+			Professor professor = professorRepository.findById(id).get();
+			professor.setNome(postProfessorDTO.getNome());
+			professor.setSalario(postProfessorDTO.getSalario());
+			professor.setStatusProfessor(postProfessorDTO.getStatusProfessor());
+			professorRepository.save(professor);
+			return new ModelAndView("redirect:/professores/" + professor.getId());
+		} catch (Exception e) {
+			System.out.println("####################################");
+			System.out.println(e.getMessage());
+			System.out.println("####################################");
+			ModelAndView mv = new ModelAndView("/professor/editar");
+			mv.addObject("listaStatusProfessor", StatusProfessor.values());
+			return mv;
+		}
 	}
 }
