e98ca1dbdf0666b52958cbbf832b1b75681f530f
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-09-18 09:58:08 -0300

    Configurando o atributo DTO para cada método

--- springprojeto/src/main/java/com/cursomvc/springprojeto/controllers/ProfessorController.java
+++ springprojeto/src/main/java/com/cursomvc/springprojeto/controllers/ProfessorController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,10 +25,10 @@ public class ProfessorController {
 	@Autowired
 	ModelMapper mapper;
 	
-	@ModelAttribute(value = "postProfessorDTO")
-	public PostProfessorDTO getPostProfessorDTO(){
-	    return new PostProfessorDTO();
-	}
+	//@ModelAttribute(value = "postProfessorDTO")
+	//public PostProfessorDTO getPostProfessorDTO(){
+	//    return new PostProfessorDTO();
+	//}
 	
 	@GetMapping("/professores")
 	public ModelAndView index() {
@@ -43,7 +42,7 @@ public class ProfessorController {
 	}
 	
 	@GetMapping("/professores/novo")
-	public ModelAndView inserirProfessor() {
+	public ModelAndView inserirProfessor(PostProfessorDTO postProfessorDTO) {
 		
 		ModelAndView mv = new ModelAndView("professor/novo");
 		mv.addObject("listaStatusProfessor", StatusProfessor.values());
@@ -51,7 +50,7 @@ public class ProfessorController {
 	}
 	
 	@PostMapping("/professores")
-	public ModelAndView create(@Valid PostProfessorDTO dto, //Vai ser avaliado
+	public ModelAndView create(@Valid PostProfessorDTO postProfessorDTO, //Vai ser avaliado
 								BindingResult bindingResult) { //objeto que registra se houve algum esso na requisicao
 		if(bindingResult.hasErrors()) {
 			
@@ -60,7 +59,7 @@ public class ProfessorController {
 			return mv;
 		}else {
 			Professor professor = new Professor();
-			professor = mapper.map(dto, professor.getClass());
+			professor = mapper.map(postProfessorDTO, professor.getClass());
 			professorRepository.save(professor);
 			
 			return new ModelAndView("redirect:/professores");
